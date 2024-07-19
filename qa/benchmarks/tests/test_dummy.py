56dbad627939570eae3f08c6ae2c8e6f970bae00
Stefaan Lippens <stefaan.lippens@vito.be>
2024-07-19 15:28:53 +0200

    Issue #5 create test report and use basic GA upload

---
+++ qa/benchmarks/tests/test_dummy.py
@@ -0,0 +1,15 @@
+"""
+Dummy tests to allow setting up additional test tooling
+"""
+
+
+def test_dummy():
+    x = 3
+    y = 5
+    assert x + y == 7
+
+
+def test_dummy2():
+    x = 3
+    y = 5
+    assert x + y == 8
