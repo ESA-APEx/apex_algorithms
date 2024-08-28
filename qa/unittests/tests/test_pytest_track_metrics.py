8d03e9dedc2a26a8431e0a4834eea7a4d96b6203
Stefaan Lippens <stefaan.lippens@vito.be>
2024-08-28 10:42:37 +0200

    Issue #7 track_metrics: also support local parquet

--- qa/unittests/tests/test_pytest_track_metrics.py
+++ qa/unittests/tests/test_pytest_track_metrics.py
@@ -70,6 +70,68 @@ def test_track_metric_json(pytester: pytest.Pytester, tmp_path):
     ]
 
 
+def test_track_metric_parquet_local(pytester: pytest.Pytester, tmp_path):
+    pytester.makeconftest(
+        """
+        pytest_plugins = [
+            "apex_algorithm_qa_tools.pytest_track_metrics",
+        ]
+        """
+    )
+    pytester.makepyfile(
+        test_addition="""
+            import pytest
+
+            @pytest.mark.parametrize("x", [5, 6])
+            def test_3plus(track_metric, x):
+                track_metric("x squared", x * x)
+                assert 3 + x == 8
+        """
+    )
+
+    metrics_path = tmp_path / "metrics.json"
+
+    start_time = time.time()
+    run_result = pytester.runpytest_subprocess(
+        f"--track-metrics-parquet={metrics_path}",
+    )
+
+    run_result.stdout.re_match_lines(
+        [r"Plugin `track_metrics` is active, reporting to"]
+    )
+    run_result.assert_outcomes(passed=1, failed=1)
+
+    assert metrics_path.exists()
+    run_result.stdout.re_match_lines([f".*Generated.*{re.escape(str(metrics_path))}.*"])
+
+    table = pyarrow.parquet.read_table(metrics_path)
+
+    df = table.to_pandas()
+    assert set(df.columns) == {
+        "nodeid",
+        "outcome",
+        "duration",
+        "start",
+        "stop",
+        "x squared",
+    }
+    df = df.set_index("nodeid")
+    assert df.loc["test_addition.py::test_3plus[5]"].to_dict() == {
+        "outcome": "passed",
+        "duration": pytest.approx(0, abs=1),
+        "start": pytest.approx(start_time, abs=1),
+        "stop": pytest.approx(start_time, abs=1),
+        "x squared": 25,
+    }
+    assert df.loc["test_addition.py::test_3plus[6]"].to_dict() == {
+        "outcome": "failed",
+        "duration": pytest.approx(0, abs=1),
+        "start": pytest.approx(start_time, abs=1),
+        "stop": pytest.approx(start_time, abs=1),
+        "x squared": 36,
+    }
+
+
 def test_track_metric_parquet_s3(
     pytester: pytest.Pytester, moto_server, s3_client, s3_bucket, monkeypatch
 ):
