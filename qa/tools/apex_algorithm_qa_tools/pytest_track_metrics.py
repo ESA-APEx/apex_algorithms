885b0237ddff9341267e57c00e6326dfefd30155
Stefaan Lippens <stefaan.lippens@vito.be>
2024-08-28 10:42:37 +0200

    Issue #7 track_metrics: also support local parquet

--- qa/tools/apex_algorithm_qa_tools/pytest_track_metrics.py
+++ qa/tools/apex_algorithm_qa_tools/pytest_track_metrics.py
@@ -21,14 +21,20 @@ Usage:
     ...
 
 -   Run the tests with desired configuration through CLI options and env vars:
-    - CLI option to set JSON file to store the metrics to:
+    - CLI option to store metrics to (local) JSON file:
       `--track-metrics-json=path/to/metrics.json`
-    - S3 credentials with env vars `APEX_ALGORITHMS_S3_ACCESS_KEY_ID`
-      and `APEX_ALGORITHMS_S3_SECRET_ACCESS_KEY`
-      (Note that the classic `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY`
-      are also supported as fallback)
-    - S3 endpoint URL with env var `APEX_ALGORITHMS_S3_ENDPOINT_URL`
-      (Note that the classic `AWS_ENDPOINT_URL` is also supported as fallback).
+    - CLI option to store metrics to local Parquet file:
+      `--track-metrics-parquet=path/to/metrics.parquet`
+    - CLI options to store metrics to Parquet file on S3:
+      `--track-metrics-parquet-s3-bucket=BUCKET` to set the S3 bucket
+      and `--track-metrics-parquet-s3-key=KEY` to specify the key/path in the bucket.
+      Additional env vars to further configure S3 access:
+        - S3 credentials with env vars `APEX_ALGORITHMS_S3_ACCESS_KEY_ID`
+        and `APEX_ALGORITHMS_S3_SECRET_ACCESS_KEY`
+        (Note that the classic `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY`
+        are also supported as fallback)
+        - S3 endpoint URL with env var `APEX_ALGORITHMS_S3_ENDPOINT_URL`
+        (Note that the classic `AWS_ENDPOINT_URL` is also supported as fallback).
 """
 
 import dataclasses
@@ -56,6 +62,11 @@ def pytest_addoption(parser: pytest.Parser):
         help="Path to JSON file to store test/benchmark metrics.",
     )
     parser.addoption(
+        "--track-metrics-parquet",
+        metavar="PATH",
+        help="Path to local Parquet file to store test/benchmark metrics.",
+    )
+    parser.addoption(
         "--track-metrics-parquet-s3-bucket",
         metavar="BUCKET",
         help="S3 bucket to use for Parquet storage of metrics.",
@@ -75,6 +86,7 @@ def pytest_configure(config):
 
     track_metrics_json = config.getoption("--track-metrics-json")
 
+    track_metrics_parquet = config.getoption("--track-metrics-parquet")
     track_metrics_parquet_s3_bucket = config.getoption(
         "--track-metrics-parquet-s3-bucket"
     )
@@ -82,14 +94,17 @@ def pytest_configure(config):
         "--track-metrics-parquet-s3-key", _S3_KEY_DEFAULT
     )
 
-    if track_metrics_json or track_metrics_parquet_s3_bucket:
+    if track_metrics_json or track_metrics_parquet or track_metrics_parquet_s3_bucket:
         config.pluginmanager.register(
             TrackMetricsReporter(
                 json_path=track_metrics_json,
+                parquet_local=track_metrics_parquet,
                 parquet_s3=_ParquetS3StorageSettings(
                     bucket=track_metrics_parquet_s3_bucket,
                     key=track_metrics_parquet_s3_key,
-                ),
+                )
+                if track_metrics_parquet_s3_bucket
+                else None,
             ),
             name=_TRACK_METRICS_PLUGIN_NAME,
         )
@@ -97,7 +112,7 @@ def pytest_configure(config):
 
 @dataclasses.dataclass(frozen=True)
 class _ParquetS3StorageSettings:
-    bucket: str | None = None
+    bucket: str
     key: str = _S3_KEY_DEFAULT
 
 
@@ -105,10 +120,12 @@ class TrackMetricsReporter:
     def __init__(
         self,
         json_path: Union[None, str, Path] = None,
+        parquet_local: Union[None, str, Path] = None,
         parquet_s3: Optional[_ParquetS3StorageSettings] = None,
         user_properties_key: str = "track_metrics",
     ):
         self._json_path = Path(json_path) if json_path else None
+        self._parquet_local = Path(parquet_local) if parquet_local else None
         self._parquet_s3 = parquet_s3
         self._suite_metrics: List[dict] = []
         self._user_properties_key = user_properties_key
@@ -132,16 +149,18 @@ class TrackMetricsReporter:
         if self._json_path:
             self._write_json_report(self._json_path)
 
-        if self._parquet_s3.bucket:
-            # TODO: also support local parquet file (without partitioning), e.g. for test/debug reasons
-            self._write_parquet_report(self._parquet_s3)
+        if self._parquet_local:
+            self._write_parquet_local(self._parquet_local)
+
+        if self._parquet_s3:
+            self._write_parquet_s3(self._parquet_s3)
 
     def _write_json_report(self, path: Union[str, Path]):
         with Path(path).open("w", encoding="utf8") as f:
             json.dump(self._suite_metrics, f, indent=2)
 
-    def _write_parquet_report(self, parquet_s3: _ParquetS3StorageSettings):
-        # Compile all (free-form) metrics into a more rigid table
+    def _to_pyarrow_table(self) -> pyarrow.Table:
+        """Compile all (free-form) metrics into a more rigid table"""
         columns = set()
         suite_metrics = []
         for m in self._suite_metrics:
@@ -164,7 +183,14 @@ class TrackMetricsReporter:
         table = pyarrow.Table.from_pydict(
             {col: [m.get(col) for m in suite_metrics] for col in columns}
         )
+        return table
 
+    def _write_parquet_local(self, parquet_local: Path):
+        table = self._to_pyarrow_table()
+        pyarrow.parquet.write_table(table=table, where=parquet_local)
+
+    def _write_parquet_s3(self, parquet_s3: _ParquetS3StorageSettings):
+        table = self._to_pyarrow_table()
         fs = pyarrow.fs.S3FileSystem(
             access_key=os.environ.get("APEX_ALGORITHMS_S3_ACCESS_KEY_ID"),
             secret_key=os.environ.get("APEX_ALGORITHMS_S3_SECRET_ACCESS_KEY"),
@@ -179,12 +205,14 @@ class TrackMetricsReporter:
         )
 
     def pytest_report_header(self):
-        return f"Plugin `track_metrics` is active, reporting to json={self._json_path}, parquet={self._parquet_s3}"
+        return f"Plugin `track_metrics` is active, reporting to json={self._json_path}, parquet_local={self._parquet_local}, parquet_s3={self._parquet_s3}"
 
     def pytest_terminal_summary(self, terminalreporter):
         reports = []
         if self._json_path:
             reports.append(str(self._json_path))
+        if self._parquet_local:
+            reports.append(str(self._parquet_local))
         if self._parquet_s3:
             reports.append(str(self._parquet_s3))
         if reports:
