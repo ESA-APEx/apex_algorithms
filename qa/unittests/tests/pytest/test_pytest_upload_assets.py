0a110b2ce30c149d21869d7bd16fb5ef1d616570
Stefaan Lippens <stefaan.lippens@vito.be>
2025-06-06 16:51:49 +0200

    Make pytest_upload_assets more xdist-compatible
    ref: eu-cdse/openeo-cdse-infra#566

--- qa/unittests/tests/pytest/test_pytest_upload_assets.py
+++ qa/unittests/tests/pytest/test_pytest_upload_assets.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-def test_basic_upload_on_fail(
+def test_upload_on_fail_basic(
     pytester: pytest.Pytester, moto_server, s3_client, s3_bucket, monkeypatch
 ):
     pytester.makeconftest(
@@ -44,7 +44,7 @@ def test_basic_upload_on_fail(
     run_result.stdout.re_match_lines(
         [
             r".*upload_assets summary",
-            r"- stats: \{'uploaded': 1\}",
+            r"- stats: \{.*'uploaded': 1.*\}",
             r"\s+-\s+'hello.txt' uploaded to 'http://.*?/test-bucket-\w+/test-run-123!test_file_maker.py__test_fail_and_upload!hello.txt'",
         ]
     )
@@ -87,6 +87,59 @@ def test_nop_on_success(
     run_result.stdout.re_match_lines(
         [
             r".*upload_assets summary",
-            r"- stats: \{'uploaded': 0\}",
+            r"- stats: \{.*'uploaded': 0.*\}",
+        ]
+    )
+
+
+def test_upload_on_fail_xdist(
+    pytester: pytest.Pytester, moto_server, s3_client, s3_bucket, monkeypatch
+):
+    pytester.makeconftest(
+        """
+        pytest_plugins = [
+            "apex_algorithm_qa_tools.pytest.pytest_upload_assets",
+        ]
+        """
+    )
+    pytester.makepyfile(
+        test_file_maker="""
+            def test_fail_and_upload(upload_assets_on_fail, tmp_path):
+                path = tmp_path / "hello.txt"
+                path.write_text("Hello world.")
+                upload_assets_on_fail(path)
+                assert 3 == 5
+        """
+    )
+
+    monkeypatch.setenv("APEX_ALGORITHMS_S3_ENDPOINT_URL", moto_server)
+    monkeypatch.setenv("APEX_ALGORITHMS_RUN_ID", "test-run-123")
+
+    run_result = pytester.runpytest_subprocess(
+        f"--upload-assets-s3-bucket={s3_bucket}",
+        "--numprocesses=2",
+    )
+    run_result.stdout.re_match_lines(
+        [
+            r"Plugin `upload_assets` is active, with upload to 'test-bucket-",
+            r"2 workers \[1 item\]",
+        ]
+    )
+    run_result.assert_outcomes(failed=1)
+
+    object_listing = s3_client.list_objects(Bucket=s3_bucket)
+    assert len(object_listing["Contents"])
+    keys = [obj["Key"] for obj in object_listing["Contents"]]
+    expected_key = "test-run-123!test_file_maker.py__test_fail_and_upload!hello.txt"
+    assert keys == [expected_key]
+
+    actual = s3_client.get_object(Bucket=s3_bucket, Key=expected_key)
+    assert actual["Body"].read().decode("utf8") == "Hello world."
+
+    run_result.stdout.re_match_lines(
+        [
+            r".*upload_assets summary",
+            r"- stats: \{.*'uploaded': 1.*\}",
+            r"\s+-\s+'hello.txt' uploaded to 'http://.*?/test-bucket-\w+/test-run-123!test_file_maker.py__test_fail_and_upload!hello.txt'",
         ]
     )
