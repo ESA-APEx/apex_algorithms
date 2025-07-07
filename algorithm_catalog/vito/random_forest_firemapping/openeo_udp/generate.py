6bc4788c32c8b5c63eaa4204c4b52d084e1d8f3b
Pratichhya <39898768+Pratichhya@users.noreply.github.com>
2025-07-07 16:13:05 +0200

    updated the RF udp

--- algorithm_catalog/vito/random_forest_firemapping/openeo_udp/generate.py
+++ algorithm_catalog/vito/random_forest_firemapping/openeo_udp/generate.py
@@ -4,10 +4,12 @@ import openeo
 from openeo.api.process import Parameter
 from openeo.rest.udp import build_process_dict
 
+from eo_extractor import s1_features , s2_features
+
 
 def generate() -> dict:
 
-    conn = openeo.connect("openeo.vito.be").authenticate_oidc()
+    connection  = openeo.connect("openeo.vito.be").authenticate_oidc()
 
     # define input parameter
     spatial_extent = Parameter.spatial_extent(
@@ -19,21 +21,28 @@ def generate() -> dict:
         name = "temporal_extent", 
         description = "Temporal extent specified as two-element array with start and end date/date-time."
         )
-    # Load s2 bands and set max cloud cover to be less than 10%
-    s2_bands = conn.load_collection(
-        collection_id="SENTINEL2_L2A",
-        spatial_extent=spatial_extent,
-        temporal_extent=temporal_extent,
-        bands=["B04", "B08"],
-        max_cloud_cover=10,
+
+    # Load s1 and s2 features 
+    s1_feature_cube = s1_features(connection,temporal_extent,spatial_extent,"median")
+    s2_feature_cube = s2_features(connection,temporal_extent,spatial_extent,"median")
+    # Merge the two feature cubes
+    inference_cube = s2_feature_cube.merge_cubes(s1_feature_cube)
+
+    # link to the trained model: this model has an expiry date
+    model = "https://openeo.vito.be/openeo/1.2/jobs/j-250707130527460a823273791daa4344/results/assets/ZWNjZTlmZWEwNGI4YzljNzZhYzc2YjQ1YjZiYTAwYzIwZjIxMWJkYTQ4NTZjMTRhYTQ0NzViOGU4ZWQ0MzNjZEBlZ2kuZXU=/2c00312531cc55303c14ba68ff2472d4/randomforest.model.tar.gz?expires=1752502120"
+
+    # predict of training data
+    inference = inference_cube.predict_random_forest(
+        model=model,
+        dimension="bands"
     )
 
     # Build the process dictionary
     return build_process_dict(
-        process_graph=s2_bands,
-        process_id="s2_bands",
-        summary="s2_bands",
-        description="s2_bands",
+        process_graph=inference,
+        process_id="random_forest_firemapping",
+        summary="Forest Fire Mapping Using Random Forest in openEO",
+        description="Forest fire mapping is a critical tool for environmental monitoring and disaster management, enabling the timely detection and assessment of burned areas. This service is build upon techniques described in the research paper by Zhou, Bao et al., which introduces a machine learning–based approach using Sentinel-2 imagery. Their method combines spectral, topographic, and textural features to improve classification accuracy, particularly emphasising GLCM texture features extracted from Sentinel-2's short-wave infrared band.Thus, the UDP performs forest fire mapping using a pre-trained Random Forest model in openEO. It combines Sentinel-1 and Sentinel-2 features, applies the model, and outputs the predicted fire mapping results.",
         parameters=[spatial_extent, temporal_extent]
     )
 
