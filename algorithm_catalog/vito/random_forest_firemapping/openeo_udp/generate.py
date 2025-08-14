802407a95c6376e72c2323f44dad20d7411cdee7
Pratichhya <39898768+Pratichhya@users.noreply.github.com>
2025-08-14 08:28:29 +0200

    updated link to the model and new parameter defined

--- algorithm_catalog/vito/random_forest_firemapping/openeo_udp/generate.py
+++ algorithm_catalog/vito/random_forest_firemapping/openeo_udp/generate.py
@@ -21,10 +21,15 @@ def generate() -> dict:
         name = "temporal_extent", 
         description = "Temporal extent specified as two-element array with start and end date/date-time."
         )
+    padding_window_size = Parameter.integer(
+        name = "padding_window_size",
+        description = "Padding window size for GLCM computation. Eg. when padding_window_size is set to 33, it refers to a 33x33 pixel window. 32 up/down or left/right pixels are added.",
+        default = 33
+    )
 
-    # Load s1 and s2 features 
-    s1_feature_cube = s1_features(connection,temporal_extent,spatial_extent,"median")
-    s2_feature_cube = s2_features(connection,temporal_extent,spatial_extent,"median")
+    # Load s1 and s2 features
+    s1_feature_cube = s1_features(connection, temporal_extent, spatial_extent, "median")
+    s2_feature_cube = s2_features(connection, temporal_extent, spatial_extent, "median", padding_window_size)
     # Merge the two feature cubes
     inference_cube = s2_feature_cube.merge_cubes(s1_feature_cube)
 
@@ -43,7 +48,7 @@ def generate() -> dict:
         process_id="random_forest_firemapping",
         summary="Forest Fire Mapping Using Random Forest in openEO",
         description="Forest fire mapping is a critical tool for environmental monitoring and disaster management, enabling the timely detection and assessment of burned areas. This service is build upon techniques described in the research paper by Zhou, Bao et al., which introduces a machine learning–based approach using Sentinel-2 imagery. Their method combines spectral, topographic, and textural features to improve classification accuracy, particularly emphasising GLCM texture features extracted from Sentinel-2's short-wave infrared band.Thus, the UDP performs forest fire mapping using a pre-trained Random Forest model in openEO. It combines Sentinel-1 and Sentinel-2 features, applies the model, and outputs the predicted fire mapping results.",
-        parameters=[spatial_extent, temporal_extent]
+        parameters=[spatial_extent, temporal_extent, padding_window_size]
     )
 
 
