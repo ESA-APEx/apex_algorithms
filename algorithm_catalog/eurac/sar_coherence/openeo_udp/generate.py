e9e86066d2874b6b86b7dc7ea42cf735093d9fdb
Emile Sonneveld <emile.sonneveld@vito.be>
2026-03-26 18:50:31 +0100

    WIP parse CWL to make UDP.

--- algorithm_catalog/eurac/sar_coherence/openeo_udp/generate.py
+++ algorithm_catalog/eurac/sar_coherence/openeo_udp/generate.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+import yaml
 
 import openeo
 from openeo.api.process import Parameter
@@ -7,88 +8,79 @@ from openeo.rest.udp import build_process_dict
 from openeo.rest.stac_resource import StacResource
 from openeo.internal.graph_building import PGNode
 
+cwl_url = "https://raw.githubusercontent.com/cloudinsar/s1-workflows/refs/heads/main/cwl/sar_coherence_parallel_temporal_extent.cwl"
 
-CWL_PATH = "cwl/sar_coherence_parallel_temporal_extent.cwl"
-CWL_URL = "https://raw.githubusercontent.com/cloudinsar/s1-workflows/refs/heads/main/" + CWL_PATH
+
+def get_cwl_inputs(cwl_yaml):
+    cwl_graph = cwl_yaml.get("$graph", [])
+    cwl_main = next((item for item in cwl_graph if item.get("id") == "main"), None)
+    return cwl_main.get("inputs", [])
+
+
+def cwl_input_to_parameter(name, cwl_input_yaml):
+    input_type = cwl_input_yaml.get("type")
+    arguments = {"name": name}
+    if input_type.endswith("?"):
+        input_type = input_type[:-1]
+        arguments["optional"] = True
+
+    if default := cwl_input_yaml.get("default"):
+        arguments["default"] = default
+
+    if doc := cwl_input_yaml.get("doc"):
+        arguments["description"] = doc
+
+    # todo, enum
+    if name == "sub_swath":
+        return Parameter(
+            schema={"type": "string", "enum": ["IW1", "IW2", "IW3", "EW1", "EW2", "EW3", "EW4", "EW5"]},
+            **arguments,
+        )
+    if name == "polarization":
+        return Parameter(
+            schema={"type": "string", "enum": ["VV", "VH", "HH", "HV"]},
+            **arguments,
+        )
+    if name == "spatial_extent":
+        return Parameter.spatial_extent(**arguments)
+    if name == "temporal_extent":
+        return Parameter.temporal_interval(**arguments)
+    elif input_type == "string":
+        return Parameter.string(**arguments)
+    elif input_type == "int":
+        return Parameter.integer(**arguments)
+    else:
+        print(f"input_type not found: {input_type}")
+        return Parameter(**arguments)
+
+
+def cwl_input_to_parameters(cwl_input_yaml):
+    parameters = []
+    for key, value in cwl_input_yaml.items():
+        parameters.append(cwl_input_to_parameter(key, value))
+    return parameters
 
 
 def generate():
+    from urllib.request import urlopen
+
+    cwl_yaml = yaml.safe_load(urlopen(cwl_url).read().decode('utf-8'))
+    cwl_inputs = get_cwl_inputs(cwl_yaml)
+    parameters = cwl_input_to_parameters(cwl_inputs)
+
+
+    context = {}
+    for parameter in parameters:
+        context[parameter.name] = {"from_parameter": parameter.name}
+
     connection = openeo.connect("openeofed.dataspace.copernicus.eu").authenticate_oidc()
-
-    spatial_extent = Parameter.spatial_extent(
-        name="spatial_extent",
-        description=(
-            "Limits the data to process to the specified bounding box or polygons. "
-            "Used to find the Sentinel-1 burst to process when `burst_id` is not provided."
-        ),
-        optional=True,
-        default=None,
-    )
-
-    temporal_extent = Parameter.temporal_interval(
-        name="temporal_extent",
-        description="Date range to search for Sentinel-1 acquisitions, e.g. ['2024-08-01', '2024-09-30'].",
-    )
-
-    temporal_baseline = Parameter.integer(
-        name="temporal_baseline",
-        description="Temporal baseline in days between InSAR pairs (e.g. 6 or 12).",
-    )
-
-    polarization = Parameter.string(
-        name="polarization",
-        description="SAR polarization channel to process.",
-        default="vv",
-        values=["vv", "vh"],
-    )
-
-    sub_swath = Parameter.string(
-        name="sub_swath",
-        description="Sentinel-1 IW sub-swath to process.",
-        default="IW2",
-        values=["IW1", "IW2", "IW3"],
-    )
-
-    burst_id = Parameter.integer(
-        name="burst_id",
-        description=(
-            "Sentinel-1 burst ID to process. "
-            "When omitted, the burst is selected automatically from `spatial_extent`."
-        ),
-        optional=True,
-        default=None,
-    )
-
-    coherence_window_rg = Parameter.integer(
-        name="coherence_window_rg",
-        description="Coherence estimation window size in the range direction.",
-        default=10,
-        optional=True,
-    )
-
-    coherence_window_az = Parameter.integer(
-        name="coherence_window_az",
-        description="Coherence estimation window size in the azimuth direction.",
-        default=2,
-        optional=True,
-    )
-
     stac_resource = StacResource(
         graph=PGNode(
             "run_cwl_to_stac",
             namespace=None,
             arguments={
-                "cwl": CWL_URL,
-                "context": {
-                    "temporal_extent": {"from_parameter": "temporal_extent"},
-                    "temporal_baseline": {"from_parameter": "temporal_baseline"},
-                    "spatial_extent": {"from_parameter": "spatial_extent"},
-                    "burst_id": {"from_parameter": "burst_id"},
-                    "polarization": {"from_parameter": "polarization"},
-                    "sub_swath": {"from_parameter": "sub_swath"},
-                    "coherence_window_rg": {"from_parameter": "coherence_window_rg"},
-                    "coherence_window_az": {"from_parameter": "coherence_window_az"},
-                },
+                "cwl": cwl_url,
+                "context": context,
             },
         ),
         connection=connection,
@@ -97,17 +89,8 @@ def generate():
     return build_process_dict(
         process_graph=stac_resource,
         process_id="sar_coherence",
-        description=(Path(__file__).parent.parent / "README.md").read_text(),
-        parameters=[
-            spatial_extent,
-            temporal_extent,
-            temporal_baseline,
-            polarization,
-            sub_swath,
-            burst_id,
-            coherence_window_rg,
-            coherence_window_az,
-        ],
+        description=cwl_yaml.get("doc"),
+        parameters=parameters,
     )
 
 
