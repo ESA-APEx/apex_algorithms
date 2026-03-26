8247e9138db46bf6eb24e3657ccd4a51b8f5b883
Paul Karlshoefer <paul.karlshoefer@dlr.de>
2026-03-26 17:50:58 +0100

    test fix

--- algorithm_catalog/worldsoils/openeo_udp/test_of_mask_dilation.py
+++ algorithm_catalog/worldsoils/openeo_udp/test_of_mask_dilation.py
@@ -181,6 +181,7 @@ def composite(con: Connection,
     ).resample_cube_spatial(s2_cube, method="near")
 
     s2_cube = s2_cube.reduce_dimension(dimension='t', reducer="first")
+    scl = scl.reduce_dimension(dimension='t', reducer="first")
     # s2_cube = s2_cube.resample_spatial(resolution=20, method="average")
     
     # s2_cube = s2_cube.reduce_dimension(dimension='t', reducer="first").band("B02").multiply(1.0)
