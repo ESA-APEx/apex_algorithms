e20f0d0fd136afbf627e32a21ae0143f246d9153
Paul Karlshoefer <paul.karlshoefer@dlr.de>
2026-03-27 08:21:15 +0100

    test fix

--- algorithm_catalog/worldsoils/openeo_udp/test_of_mask_dilation.py
+++ algorithm_catalog/worldsoils/openeo_udp/test_of_mask_dilation.py
@@ -203,13 +203,13 @@ def composite(con: Connection,
     # b02_original = s2_cube
     # return b02_original
     b02_0 = s2_cube.mask(cond_scl_cloud)
-    # b02_0 = b02_0.reduce_dimension(dimension='t', reducer="first")
+    b02_0 = b02_0.reduce_dimension(dimension='t', reducer="first")
     # return b02_0
     dilated_mask = cond_scl_cloud.apply_kernel(kernel=kernel)
     dilated_mask = (dilated_mask > 0.001)
     b02_1 = s2_cube.mask(dilated_mask)
-    ret = b02_0.merge_cubes(b02_1)
-    return dilated_mask.reduce_dimension(dimension='t', reducer="first")
+    # ret = b02_0.merge_cubes(b02_1)
+    # return dilated_mask.reduce_dimension(dimension='t', reducer="first")
     b02_1 = b02_1.reduce_dimension(dimension='t', reducer="first")
     b02_1 = b02_1.add_dimension(name="bands", label="B02_d")
     ret = b02_0.merge_cubes(b02_1)
