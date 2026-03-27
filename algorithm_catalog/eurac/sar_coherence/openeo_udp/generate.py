dc1c0560ba21f1ed2628181d808f0982223b003b
Emile Sonneveld <emile.sonneveld@vito.be>
2026-03-27 14:05:48 +0100

    Add "default": null to try avoiding "Process 'sar_coherence' parameter 'spatial_extent' is required."

--- algorithm_catalog/eurac/sar_coherence/openeo_udp/generate.py
+++ algorithm_catalog/eurac/sar_coherence/openeo_udp/generate.py
@@ -1,80 +1,19 @@
 import json
-import yaml
 
 import openeo
+import yaml
 from openeo.api.process import Parameter
-from openeo.rest.udp import build_process_dict
-from openeo.rest.stac_resource import StacResource
 from openeo.internal.graph_building import PGNode
+from openeo.rest.stac_resource import StacResource
+from openeo.rest.udp import build_process_dict
 
-cwl_url = "https://raw.githubusercontent.com/cloudinsar/s1-workflows/refs/heads/main/cwl/sar_coherence_parallel_temporal_extent.cwl"
+from esa_apex_toolbox.cwl_to_udp_utils import get_cwl_main, get_cwl_inputs, cwl_input_to_parameters
 
-
-def get_cwl_main(cwl_yaml):
-    cwl_graph = cwl_yaml.get("$graph", None)
-    if cwl_graph is None:
-        return cwl_yaml
-    else:
-        return next((item for item in cwl_graph if item.get("id") == "main"), None)
+cwl_url = "https://raw.githubusercontent.com/cloudinsar/s1-workflows/refs/heads/udp/cwl/sar_coherence_parallel_temporal_extent.cwl"
+# cwl_url = "https://raw.githubusercontent.com/cloudinsar/s1-workflows/refs/heads/udp/cwl/sar_interferogram.cwl"
 
 
-def get_cwl_inputs(cwl_yaml):
-    return get_cwl_main(cwl_yaml).get("inputs", [])
-
-
-def cwl_input_to_parameter(name, cwl_input_yaml):
-    """
-    Convert to Parameter object that openEO can use.
-    Not all properties are converted. For example enum values are not.
-    """
-    if isinstance(cwl_input_yaml, list) and len(cwl_input_yaml) > 0:
-        cwl_input_yaml = cwl_input_yaml[0]
-    assert isinstance(cwl_input_yaml, dict)
-    input_type = cwl_input_yaml.get("type")
-    arguments = {"name": name}
-    if isinstance(input_type, dict):
-        input_type = cwl_input_yaml.get("type").get("type")
-
-    if input_type.endswith("?"):
-        input_type = input_type[:-1]
-        arguments["optional"] = True
-
-    if default := cwl_input_yaml.get("default"):
-        arguments["default"] = default
-
-    if doc := cwl_input_yaml.get("doc"):
-        arguments["description"] = doc
-
-    # todo, enum values
-    if name == "spatial_extent":
-        return Parameter.spatial_extent(**arguments)
-    if name == "temporal_extent":
-        return Parameter.temporal_interval(**arguments)
-    elif input_type == "string":
-        return Parameter.string(**arguments)
-    elif input_type == "int":
-        return Parameter.integer(**arguments)
-    elif input_type == "enum":
-        schema = {"type": "string"}
-        if symbols := cwl_input_yaml.get("symbols"):
-            schema["enum"] = symbols
-        return Parameter(
-            **arguments,
-            schema=schema,
-        )
-    else:
-        print(f"input_type not found: {input_type}")
-        return Parameter(**arguments)
-
-
-def cwl_input_to_parameters(cwl_input_yaml):
-    parameters = []
-    for key, value in cwl_input_yaml.items():
-        parameters.append(cwl_input_to_parameter(key, value))
-    return parameters
-
-
-def generate():
+def generate() -> dict:
     from urllib.request import urlopen
 
     cwl_yaml = yaml.safe_load(urlopen(cwl_url).read().decode('utf-8'))
@@ -107,5 +46,6 @@ def generate():
 
 
 if __name__ == "__main__":
+    j = generate()
     with open("sar_coherence.json", "w") as f:
-        json.dump(generate(), f, indent=2)
+        json.dump(j, f, indent=2)
