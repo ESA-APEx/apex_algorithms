3b0e56df77b36e0fb98697b819e15871bd95b104
Hans Vanrompay <hansvanrompay@hotmail.com>
2026-07-14 14:47:25 +0200

    moving some functions to private given experimental nature and adding unit tests

--- qa/tools/apex_algorithm_qa_tools/metrics/__init__.py
+++ qa/tools/apex_algorithm_qa_tools/metrics/__init__.py
@@ -6,15 +6,15 @@ from apex_algorithm_qa_tools.metrics.parquet_metrics import (
     scenario_id_from_nodeid,
 )
 from apex_algorithm_qa_tools.metrics.performance_baselines import (
-    check_reference_performance,
-    compute_baselines,
-    compute_threshold_stats,
+    _check_reference_performance,
+    _compute_baselines,
+    _compute_threshold_stats,
 )
 
 __all__ = [
-    "check_reference_performance",
-    "compute_baselines",
-    "compute_threshold_stats",
+    "_check_reference_performance",
+    "_compute_baselines",
+    "_compute_threshold_stats",
     "load_recent_scenario_metrics_map",
     "load_scenario_metrics",
     "scenario_id_from_nodeid",
