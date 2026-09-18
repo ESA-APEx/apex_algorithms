ed13bd44ff5d4fd1dab5b80bd8bbb1a03584534c
bramjanssen <brm.janssen@gmail.com>
2026-09-18 13:19:27 +0200

    feat: fixes to force_level2 parameters

--- algorithm_catalog/david_frantz/force_level2/openeo_udp/generate.py
+++ algorithm_catalog/david_frantz/force_level2/openeo_udp/generate.py
@@ -8,11 +8,18 @@ def generate_arguments(parameters):
     return {param["name"]: {"from_parameter": param["name"]} for param in parameters}
 
 
+def normalize_parameter(parameter):
+    if "anyOf" in parameter["schema"]:
+        parameter["schema"] = parameter["schema"]["anyOf"]
+    return parameter
+
+
 def generate():
     connection = openeo.connect("openeo-staging.dataspace.copernicus.eu").authenticate_oidc()
 
     process = connection.describe_process("force_level2")
     process["description"] = (Path(__file__).parent / "description.md").read_text()
+    process["parameters"] = [normalize_parameter(param) for param in process["parameters"]]
     process["process_graph"] = {
         "force_level_2": {
             "process_id": "force_level_2",
--- algorithm_catalog/david_frantz/force_tsa/openeo_udp/generate.py
+++ algorithm_catalog/david_frantz/force_tsa/openeo_udp/generate.py
@@ -8,11 +8,18 @@ def generate_arguments(parameters):
     return {param["name"]: {"from_parameter": param["name"]} for param in parameters}
 
 
+def normalize_parameter(parameter):
+    if "anyOf" in parameter["schema"]:
+        parameter["schema"] = parameter["schema"]["anyOf"]
+    return parameter
+
+
 def generate():
     connection = openeo.connect("openeo-staging.dataspace.copernicus.eu").authenticate_oidc()
 
     process = connection.describe_process("force_tsa")
     process["description"] = (Path(__file__).parent / "description.md").read_text()
+    process["parameters"] = [normalize_parameter(param) for param in process["parameters"]]
     process["process_graph"] = {
         "force_tsa": {
             "process_id": "force_tsa",
